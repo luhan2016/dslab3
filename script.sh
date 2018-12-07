74453e7689d9e914216843ef5a5f9e1d98a0744f
luhan2016 <luhansophia@gmail.com>
2018-12-07 11:12:37 +0100

    add, modify, delete works, but modify & delete happens simultaneously needs to be conserndered

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in `seq 1 8`; do
+for i in `seq 1 2`; do
 	curl -d 'entry=vessel1:'${i} -X 'POST' 'http://10.1.0.1/board' &
 	curl -d 'entry=vessel2:'${i} -X 'POST' 'http://10.1.0.2/board' &
 	curl -d 'entry=vessel3:'${i} -X 'POST' 'http://10.1.0.3/board' 
